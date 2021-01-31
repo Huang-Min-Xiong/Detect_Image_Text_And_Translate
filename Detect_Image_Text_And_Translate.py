2dc21685105d333f4e99c06b269546fce0f2315a
黃敏雄 <64833384+MinXiong-Huang@users.noreply.github.com>
2021-01-31 10:18:14 +0800

    Update Detect_Image_Text_And_Translate.py

--- Detect_Image_Text_And_Translate.py
+++ Detect_Image_Text_And_Translate.py
@@ -10,11 +10,11 @@ img_path = easygui.fileopenbox()
 
 #偵測圖像文字且翻譯文字
 def Detect_image_text_and_translate():
-    img=cv2.imread(img_path)
-    text=pytesseract.image_to_string(img)
+    img = cv2.imread(img_path)
+    text = pytesseract.image_to_string(img)
     print('翻譯前:\n'+str(text)+'\n')
     translator = Translator()
-    Translation_Text=translator.translate(text,dest='zh-TW') #轉成繁體
+    Translation_Text = translator.translate(text,dest='zh-TW') #轉成繁體
     print('翻譯後:\n'+str(Translation_Text.text)+'\n')
 
     with open(r"Detect_Text\Detect_Text.txt","w") as f:
@@ -24,22 +24,22 @@ def Detect_image_text_and_translate():
 
 
 #判斷是否已選擇Img路徑
-if img_path==None:
-    Open_img_path=False 
+if img_path == None:
+    Open_img_path = False 
 else:
-    Open_img_path=True
+    Open_img_path = True
     
 
 #當選擇了Img路徑才執行功能    
-if Open_img_path==True:
+if Open_img_path == True:
     Detect_image_text_and_translate()
 
 #持續判斷使用者是否要繼續執行
-while Open_img_path==False :
+while Open_img_path == False :
     print('尚未選擇Img路徑!')
-    Input=input('若要繼續執行請輸入Y,不要則輸入N:\n')
+    Input = input('若要繼續執行請輸入Y,不要則輸入N:\n')
 
-    if Input=='y' or Input=='Y':
+    if Input == 'y' or Input == 'Y':
         img_path = easygui.fileopenbox() 
     else:
         exit  
